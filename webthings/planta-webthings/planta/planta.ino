c2bd8f9f52ace402c391fc2c152ded6b4eb707c3
Daniel <danieldelgadosan@hotmail.com>
2020-03-18 20:11:37 +0100

    Create planta.ino
    pruebas de interconexion de ambos sensores

---
+++ webthings/planta-webthings/planta/planta.ino
@@ -0,0 +1,67 @@
+#include <Arduino.h>
+#include "Thing.h"
+#include "WebThingAdapter.h"
+
+// TODO: Hardcode your wifi credentials here (and keep it private)
+const char *ssid = "delgado";
+const char *password = "micasa221b";
+
+const int ledPin = 13; // manually configure LED pin
+
+WebThingAdapter *adapter;
+
+const char *ledTypes[] = {"OnOffSwitch", "Light", nullptr};
+ThingDevice led("led", "Built-in LED", ledTypes);
+ThingProperty ledOn("on", "", BOOLEAN, "OnOffProperty");
+
+bool lastOn = false;
+
+void setup(void) {
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
+  Serial.begin(115200);
+  Serial.println("");
+  Serial.print("Connecting to \"");
+  Serial.print(ssid);
+  Serial.println("\"");
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  bool blink = true;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    digitalWrite(ledPin, blink ? LOW : HIGH); // active low led
+    blink = !blink;
+  }
+  digitalWrite(ledPin, HIGH); // active low led
+
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  adapter = new WebThingAdapter("w25", WiFi.localIP());
+
+  led.addProperty(&ledOn);
+  adapter->addDevice(&led);
+  adapter->begin();
+  Serial.println("HTTP server started");
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.print("/things/");
+  Serial.println(led.id);
+}
+
+void loop(void) {
+  adapter->update();
+  bool on = ledOn.getValue().boolean;
+  digitalWrite(ledPin, on ? LOW : HIGH); // active low led
+  if (on != lastOn) {
+    Serial.print(led.id);
+    Serial.print(": ");
+    Serial.println(on);
+  }
+  lastOn = on;
+}
